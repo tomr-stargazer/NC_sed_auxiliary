8be63b3069cdbd45f2f180190f1561f395a82d1f
Tom Rice <t.rice90@gmail.com>
2016-02-21 13:23:53 -0500

    wrote a working sed_inspect tool and it is great

---
+++ sed_inspect.py
@@ -0,0 +1,47 @@
+#!/Users/tsrice/anaconda/bin/python
+
+"""
+This is a python tool to generate a simple .png file of a given SED at the command line.
+
+"""
+
+from __future__ import division
+
+import os, sys, glob
+
+from NC_sed_auxiliary.sed_output import SED_output
+
+
+def some_function_that_does_it():
+
+    # first let's identify what file(s) to read
+    cwd = os.getcwd()
+
+    # if there were no command line arguments passed, then do all of the 17* files.
+    if len(sys.argv)<2:
+        preliminary_file_list = glob.glob(os.path.join(cwd, "17*"))
+        file_list = [file_ for file_ in preliminary_file_list if file_[-1] != "~"]
+    else:
+        preliminary_file_list = sys.argv[1:]
+        file_list = [os.path.join(cwd,file_) for file_ in preliminary_file_list if ~os.path.isabs(file_)]
+
+
+    for file_ in file_list:
+
+        path, filename_without_path = os.path.split(file_)
+        png_filename = filename_without_path.lstrip("17.") + ".png"
+
+        print "Saving PNG as: {0}".format(png_filename)
+
+        sed = SED_output.load(file_)
+        fig = sed.plot()
+        fig.savefig(os.path.join(cwd,png_filename))
+
+
+    return file_list
+
+
+if __name__ == "__main__":
+
+    some_function_that_does_it()
+    sys.exit()
